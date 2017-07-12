5350906c1dd44754ba1470db4a9924e45b05ccc5
root <thestorkbot@gmail.com>
2017-07-12 06:35:09 +0000

    Add FB messenger gem with simple bot.

---
+++ app/bot/listen.rb
@@ -0,0 +1,17 @@
+require "facebook/messenger"
+include Facebook::Messenger
+
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+
+Bot.on :message do |message|
+  Bot.deliver({
+    recipient: message.sender,
+    message: {
+      text: message.text
+    }
+  }, access_token: ENV["ACCESS_TOKEN"])
+end
+
+#Bot.on :message do |message|
+#  puts "I got your message!"
+#end

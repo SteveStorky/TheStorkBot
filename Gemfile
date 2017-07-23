2f7ed69b775ba86d56b5968a6edfaec997cb83c0
Innistrad <diki.andeas@jerbeeindonesia.com>
2017-07-24 02:29:22 +0700

    Simple prototype version

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.7'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# Use mysql as the database for Active Record
+gem 'mysql2', '>= 0.3.13', '< 0.5'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -13,6 +13,10 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+
+gem 'api-ai-ruby'
+gem 'facebook-messenger'
+gem 'dotenv-rails'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -32,10 +36,6 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-gem 'mysql2', '>= 0.3.13', '< 0.5'
-gem 'facebook-messenger'
-gem 'dotenv-rails'
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -48,4 +48,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -1,17 +1,43 @@
 require "facebook/messenger"
 include Facebook::Messenger
-
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
-
+# message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
+# message.sender      # => { 'id' => '1008372609250235' }
+# message.sent_at     # => 2016-04-22 21:30:36 +0200
+# message.text        # => 'Hello, bot!'
 Bot.on :message do |message|
+
+  # Connect to API.ai when got facebook message
+  client = ApiAiRuby::Client.new( :client_access_token => 'xxxxxxxxyyyyyyzzzzz' )
+  # Will be replace by initialize services class soon
+
+  # Send facebook message's message to API.ai
+  response = client.text_request message.text
+  action = response[:result][:action]
+  speech = response[:result][:fulfillment][:speech] || "Wait..."
+  # Will ber replaced by messaging system class soon
+
+  # Processor
+  # Will be replaced with more neat class & API connections
+
+  # Shipment Status (Dummy)
+  if action == "tracking.shipment" and response[:result][:parameters][:tracking_number] != ''
+      additionalinfo = response[:result][:parameters][:tracking_number]
+      #additionalinfo = "Oops"
+      Bot.deliver({
+  	recipient: message.sender,
+  	message: {
+  	    text: additionalinfo+" is being prepared for delivery in the delivery depot."
+  	}
+      }, access_token: ENV["ACCESS_TOKEN"])
+  end
+
+  # Reply to sender
   Bot.deliver({
     recipient: message.sender,
     message: {
-      text: message.text
+      text: speech
     }
   }, access_token: ENV["ACCESS_TOKEN"])
-end
 
-#Bot.on :message do |message|
-#  puts "I got your message!"
-#end
+end
